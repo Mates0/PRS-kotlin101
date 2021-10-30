73cb9bccecacd16d36dc0a569cbbd85d82cebcfb
Mates0 <73097527+Mates0@users.noreply.github.com>
2021-10-30 20:43:27 +0200

    Sečtení čísel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,36 @@ import java.util.*
 
 fun main() {
     val sc = Scanner(System.`in`)
-    var maxNumber = 0
-    var minNumber = 0
-
-    while (sc.hasNext()) {
-        val input = sc.nextInt()
-        if (input > maxNumber)
-            maxNumber = input
-        if (minNumber == 0)
-            minNumber = input
-        if (input < minNumber)
-            minNumber = input
+    val input = sc.nextLine()
+    var sum = 0
+    for (element in input) {
+        if (element.code == 49) {
+            sum += 1
+        }
+        if (element.code == 50) {
+            sum += 2
+        }
+        if (element.code == 51) {
+            sum += 3
+        }
+        if (element.code == 52) {
+            sum += 4
+        }
+        if (element.code == 53) {
+            sum += 5
+        }
+        if (element.code == 54) {
+            sum += 6
+        }
+        if (element.code == 55) {
+            sum += 7
+        }
+        if (element.code == 56) {
+            sum += 8
+        }
+        if (element.code == 57) {
+            sum += 9
+        }
     }
-    println("$maxNumber $minNumber")
+    println(sum)
 }
